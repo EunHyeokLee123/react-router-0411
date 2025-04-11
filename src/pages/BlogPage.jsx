05699e56404ee1e6e3625008558bc591cdee30d5
EunHyeok <secun77@naver.com>
2025-04-11 14:04:11 +0900

    초기 커밋

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainNav from '../components/MainNav';
+
+const BlogPage = () => {
+  return (
+    <>
+      <MainNav />
+      <h1>Blog Page 입니다~</h1>
+    </>
+  );
+};
+
+export default BlogPage;
